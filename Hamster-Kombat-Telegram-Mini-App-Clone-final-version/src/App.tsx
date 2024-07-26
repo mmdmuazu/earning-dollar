85f32126787888d9061624b8e036bb0aa3fdca70
mmdmuazu <162482343+mmdmuazu@users.noreply.github.com>
2024-07-26 11:27:53 +0100

    App.tsx

--- Hamster-Kombat-Telegram-Mini-App-Clone-final-version/src/App.tsx
+++ Hamster-Kombat-Telegram-Mini-App-Clone-final-version/src/App.tsx
@@ -136,7 +136,7 @@ const App: React.FC = () => {
         <div className="px-4 z-10">
           <div className="flex items-center space-x-2 pt-4">
             <div className="p-1 rounded-lg bg-[#1d2025]">
-              <Hamster size={24} className="text-[#d4d4d4]" />
+              <img src={dollar-coin} className=" h-[25px] w-[-25px] text-[#d4d4d4]" />
             </div>
             <div>
               <p className="text-sm">AMIR (CEO)</p>
